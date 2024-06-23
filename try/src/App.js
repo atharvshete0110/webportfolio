f34e536942c3c1847c7bdf984875c093a414c84b
atharvshete0110 <atharvashete0110@gmail.com>
2024-06-23 14:59:22 +0530

    Initial Commit

---
+++ try/src/App.js
@@ -0,0 +1,219 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <>
+//     <h1>hii</h1>
+//     <div classNameNameNameName="App">
+//       <header classNameNameNameName="App-header">
+//         <img src={logo} classNameNameNameName="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           classNameNameNameName="App-link"
+//           href="https://youtube.com"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React together now
+//         </a>
+//         <h1>hii</h1>
+//       </header>
+//     </div> 
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+// // import logo from './logo.svg';
+// import './App.css';
+
+
+// let name = "athatbia"
+// function App() {
+//   return (
+//    <>
+//    <nav>
+//     <li>home</li>
+//     <li>About</li>
+//     <li>contact</li>
+//    </nav>
+   
+//    <div classNameNameNameName="container">
+   
+//     <h1>hiii this is webpage {name}</h1>
+//     <p>
+//       lorem cisnciew iwnueun siwncewinc
+//     </p>
+//    </div>
+   
+   
+   
+
+
+
+//    </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// // import logo from './logo.svg';
+// import './App.css';
+// import Button from 'react-bootstrap/Button';
+
+
+
+// function App() {
+//   return (
+//    <>
+//   <nav class="navbar bg-primary" data-bs-theme="dark">
+//   <div className="container-fluid">
+//     <a className="navbar-brand" href="/">Navbar</a>
+//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+//     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+//       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//         <li className="nav-item">
+//           <a className="nav-link active" aria-current="page" href="/">Home</a>
+//         </li>
+//         <li className="nav-item">
+//           <a className="nav-link" href="/">Link</a>
+//         </li>
+//         <li className="nav-item">
+//           {/* <a className="nav-link disabled" aria-disabled="true">Disabled</a> */}
+//         </li>
+//       </ul>
+//       <form className="d-flex" role="search">
+//         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+//         <button className="btn btn-outline-success" type="submit">Search</button>
+//       </form>
+//     </div>
+//   </div>
+// </nav>
+
+// <div class="d-flex justify-content-center">
+//   <div class="spinner-border" role="status">
+//     <span class="visually-hidden">Loading...</span>
+//   </div>
+// </div>
+
+
+// <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+//     <div className="toast-header">
+//       <img src="..." class="rounded me-2" alt="..."/>
+//       <strong className="me-auto">Bootstrap</strong>
+//       <small className="text-body-secondary">2 seconds ago</small>
+//       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+//     </div>
+//     <div class="toast-body">
+//       Heads up, toasts will stack automatically
+//     </div>
+//   </div>
+
+//   <div class="text-bg-success p-3">Success with contrasting color</div>
+
+
+//   <div class="ratio ratio-16x9">
+//   <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+// </div>
+
+
+      
+//    </>
+//   );
+// }
+
+
+
+// export default App;
+
+
+
+
+
+// import logo from './logo.svg';
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+
+
+
+function App() {
+  return (
+   <>
+  <nav class="navbar bg-primary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+        <li className="nav-item">
+          {/* <a className="nav-link disabled" aria-disabled="true">Disabled</a> */}
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="d-flex justify-content-center">
+  <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
+
+
+<div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div className="toast-header">
+      <img src="..." class="rounded me-2" alt="..."/>
+      <strong className="me-auto">Bootstrap</strong>
+      <small className="text-body-secondary">2 seconds ago</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      Heads up, toasts will stack automatically
+    </div>
+  </div>
+
+  <div class="text-bg-success p-3">Success with contrasting color</div>
+
+
+  <div class="ratio ratio-16x9">
+  <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+</div>
+
+
+      
+   </>
+  );
+}
+
+
+
+export default App;
+
+
+
+
